17cb83cd26ce3f4e818231c47c9deb45b94da54b
gowtham-clouds11 <gowtham16it15@gmail.com>
2023-12-09 11:47:04 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import logo from './logo.svg';
+import './App.css';
+import Home from './pages/home/Home';
+
+function App() {
+
+  function add(){
+    return "gowtham"
+  }
+  
+  const a=10
+  return (
+   <div>
+      <Home />
+   </div>
+  );
+}
+
+export default App;
---
+++ src/pages/home/Home.jsx
@@ -0,0 +1,40 @@
+import React from "react";
+import "./Home.css";
+
+function Home(){
+    return(
+        <div>
+            <div className="header">Gowtham</div>
+            <div className="row">
+<div className="col">
+<div className="cards">
+<div className="childCards">
+    Product 1
+</div>
+<div className="childCards">
+    Product 2
+</div>
+<div className="childCards">
+    Product 3
+</div>
+<div className="childCards">
+    Product 1
+</div>
+<div className="childCards">
+    Product 2
+</div>
+<div className="childCards">
+    Product 3
+</div>
+
+    </div>
+</div>
+<div className="col">
+    column 2
+</div>
+            </div>
+        </div>
+    )
+}
+
+export default Home
